557a876319842ad043fee35025eae2468a732693
besoos <bence.p.soos@nokia.com>
2021-10-01 09:20:18 +0200

    3.1 added

---
+++ sarifele_feladatok/3.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Haromszog rajzolas\n");
+    float a, b, c;
+    printf("Elso oldalhossz:\n");
+    scanf("%f", &a);
+    printf("Masodik oldalhossz:\n");
+    scanf("%f", &b);
+    printf("Harmadik oldalhossz:\n");
+    scanf("%f", &c);
+    if ( a + b > c && a + c > b && b + c > a){ printf("Lehetseges haromszog"); } else{ printf("Nem lehetseges haromszog"); }
+    return 0;
+}
